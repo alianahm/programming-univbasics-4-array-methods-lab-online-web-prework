93d87e97b47ffc3ad54df0e27340bebb160abf04
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2019-10-02 07:12:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,14 +1,16 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array = ["5", "3", "2", "1"]
+  array.using_sort
 end
 
 def using_reverse(array)
-
-end
+ array = ["1", "4", "5", "3"]
+ array.using_reverse
+ end
 
 def using_first(array)
 
@@ -20,4 +22,8 @@ end
 
 def using_size(array)
 
+end
+
+def create_an_empty_array
+  []
 end
